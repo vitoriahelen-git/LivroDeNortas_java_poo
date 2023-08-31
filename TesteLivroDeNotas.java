fbfefe78f6c486b36fc34caec9b7e349f1fbf916
Vitoria Helen <vitoriahellen3004@gmail.com>
2023-08-31 18:10:08 -0300

    ap06(java): teste de livros

---
+++ TesteLivroDeNotas.java
@@ -0,0 +1,12 @@
+public class TesteLivroDeNotas{
+    static public void main(String args []){
+        //atribuição 
+        //declara varaivel (variavel de referencia)
+        //contrução de um objeto 
+        LivroDeNotas livroDeNotas = new LivroDeNotas(); //declaraçao de uma variavel 
+        livroDeNotas.exibirMensagem(); 
+        System.out.println("Até logo"); 
+        //. --> operador de acesso a membro 
+        //new cria um objeto (heap)
+    }
+}
