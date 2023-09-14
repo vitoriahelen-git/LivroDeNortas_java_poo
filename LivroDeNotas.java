6171f9cee3eca1e332520475ea98cb15ecfd98d6
Vitoria Helen <vitoriahellen3004@gmail.com>
2023-09-14 16:07:11 -0300

    ap06(java): fim da listagem 12.1

--- LivroDeNotas.java
+++ LivroDeNotas.java
@@ -1,5 +1,17 @@
 public class LivroDeNotas{
-    public void exibirMensagem(){
-        System.out.println("Bem Vindo ao livro de notas"); 
+    //variavel de instacia
+    //instacia == objeto
+    String nomeDisciplina; //variavel
+
+
+    public void exibirMensagem(){ //metodo
+        System.out.printf("Bem Vindo ao livro de %s\n", nomeDisciplina); 
+        //printl("Bem Vindo ao livro de %s", nomeDisciplina); 
+
+    }
+    //construtor padrão (aquele cuja a lista de paramentros é vazia)
+    LivroDeNotas(String nomeDisciplina){
+        this.nomeDisciplina = nomeDisciplina; 
+        //this.nomeDisciplina = nomeDisciplina
     }
 }
--- TesteLivroDeNotas.java
+++ TesteLivroDeNotas.java
@@ -1,12 +1,19 @@
 public class TesteLivroDeNotas{
     static public void main(String args []){
+
+        String nomeDisciplina = javax.swing.JOptionPane.showInputDialog("Digite o nome da disciplina: ");
         //atribuição 
         //declara varaivel (variavel de referencia)
         //contrução de um objeto 
-        LivroDeNotas livroDeNotas = new LivroDeNotas(); //declaraçao de uma variavel 
+        //inferencia de tipo
+        var livroDeNotas = new LivroDeNotas(nomeDisciplina); //declaraçao de uma variavel   var a = 2 (tipo int)
+        //livroDeNotas.nomeDisciplina = nomeDisciplina;
+        //enviando 
         livroDeNotas.exibirMensagem(); 
-        System.out.println("Até logo"); 
+        System.out.println("Ate logo"); 
         //. --> operador de acesso a membro 
         //new cria um objeto (heap)
     }
 }
+
+//autoco
